27d6dfea57930546088d6848bcdafc827a4833a3
Tyler Szabo <tyler.szabo@gmail.com>
2017-12-22 21:33:55 -0800

    Create start menu shortcut for winmtr-redux

---
+++ winmtr-redux/tools/chocolateyInstall.ps1
@@ -0,0 +1,22 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  url           = 'https://github.com/White-Tiger/WinMTR/releases/download/v1.0/WinMTR-v100-static.zip'
+  checksum      = '2DA85A22A319E87705FA846E949E28B9A23726D161816228D49AF402D55EA60E'
+  checksumType  = 'sha256'
+}
+
+Install-ChocolateyZipPackage @packageArgs
+
+$StartMenuDir = Get-ItemPropertyValue -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" -Name "Common Programs"
+
+if ($env:ChocolateyForceX86 -or (-not [Environment]::Is64BitOperatingSystem)) {
+  $TargetExe = Join-Path -Path $toolsDir -ChildPath "WinMTR.exe"
+} else {
+  $TargetExe = Join-Path -Path $toolsDir -ChildPath "WinMTR64.exe"
+}
+
+Install-ChocolateyShortcut -ShortcutFilePath (Join-Path -Path $StartMenuDir -ChildPath "WinMTR.lnk") -TargetPath $TargetExe -WorkingDirectory $toolsDir
---
+++ winmtr-redux/tools/chocolateyUninstall.ps1
@@ -0,0 +1,6 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$StartMenuDir = Get-ItemPropertyValue -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" -Name "Common Programs"
+
+Remove-Item -Path (Join-Path -Path $StartMenuDir -ChildPath "WinMTR.lnk")
