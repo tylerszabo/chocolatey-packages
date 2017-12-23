1696e4c17f4f36d868de18e5bf92dfe2910602d6
Tyler Szabo <tyler.szabo@gmail.com>
2017-12-22 21:14:28 -0800

    Make Invoke-ChocoPack script overwrite if input is modified

--- Invoke-ChocoPack.ps1
+++ Invoke-ChocoPack.ps1
@@ -6,7 +6,9 @@ Pack nuspec files
 .PARAMETER OutputDirectory
 Where to create .nupkg files (Default: Script Root)
 .PARAMETER Force
-Overwrite output if set
+Overwrite output, even if input has no changes
+.PARAMETER NoOverwrite
+Don't overwrite output, even if input has changes
 .EXAMPLE
 & .\Invoke-ChocoPack
 #>
@@ -20,20 +22,40 @@ Param (
   $Force = $False,
 
   [switch]
+  $NoOverwrite = $False,
+
+  [switch]
   $WhatIf = $False
 )
 
-Get-ChildItem -LiteralPath $OutputDirectory -Recurse -Filter "*.nuspec" | ForEach-Object {
+Get-ChildItem -LiteralPath $OutputDirectory -Recurse -Filter "*.nuspec" | Where-Object {
+  if ($Force) { Return $True }
+
   [xml]$NuSpecXML = $_ | Get-Content
   $PackageId = $NuSpecXML.package.metadata.id
   $PackageVersion = $NuSpecXML.package.metadata.version
-  $ComputedOutFile = "$OutputDirectory\$PackageId.$PackageVersion.nupkg"
 
-  if (-not $Force -and (Test-Path -LiteralPath $ComputedOutFile)) {
-    Write-Warning "Not overwriting $ComputedOutFile"
-  } else {
-    if ($WhatIf) { Write-Host "WhatIf: Would pack $($_.FullName) into $ComputedOutFile" } else {
-      & choco pack --outputdirectory "$OutputDirectory" "$($_.FullName)"
+  $ComputedOutFile = Get-Item -Path "$OutputDirectory\$PackageId.$PackageVersion.nupkg" -ErrorAction SilentlyContinue
+
+  if ($ComputedOutFile) {
+    $InputModTime = ($_.Directory | Get-ChildItem -Recurse | Measure-Object -Maximum -Property LastWriteTime).Maximum
+
+    if ($InputModTime -gt $ComputedOutFile.LastWriteTime) {
+      if ($NoOverwrite) {
+        Write-Warning "Not overwriting $($ComputedOutFile.FullName), even though input is newer"
+        Return $False
+      }
+    } else {
+      Return $False
     }
+
+    Write-Verbose "Overwriting $($ComputedOutFile.FullName) as input is newer"
+  }
+
+  Return $True
+
+} | ForEach-Object {
+  if ($WhatIf) { Write-Host "WhatIf: Would pack $($_.FullName)" } else {
+    & choco pack --outputdirectory "$OutputDirectory" "$($_.FullName)"
   }
 }
