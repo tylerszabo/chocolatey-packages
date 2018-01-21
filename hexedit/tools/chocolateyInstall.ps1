c335113b5d0b03fc5846998da29188a293b97664
Tyler Szabo <tyler.szabo@gmail.com>
2018-01-20 19:37:02 -0800

    Add hexedit 5.0

---
+++ hexedit/tools/chocolateyInstall.ps1
@@ -0,0 +1,18 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  url           = Join-Path -Path $toolsDir -ChildPath "HexEdit5build1349.zip"
+  checksum      = 'ada50ce79ae617996c284bec33fba1c55aa3ec21630a0a9bab091bf958b1cecc'
+  checksumType  = 'sha256'
+}
+
+Install-ChocolateyZipPackage @packageArgs
+
+$StartMenuDir = (Get-ItemProperty -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" -Name "Common Programs") | Select-Object -ExpandProperty "Common Programs"
+
+$TargetExe = Join-Path -Path $toolsDir -ChildPath "HexEdit.exe"
+
+Install-ChocolateyShortcut -ShortcutFilePath (Join-Path -Path $StartMenuDir -ChildPath "HexEdit.lnk") -TargetPath $TargetExe -WorkingDirectory $toolsDir
