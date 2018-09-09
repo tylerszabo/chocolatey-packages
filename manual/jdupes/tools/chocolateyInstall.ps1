4492466875eba889c4139a099cfadbdfa7fb57a6
Tyler Szabo <tyler.szabo@gmail.com>
2018-09-08 17:49:25 -0700

    Update jdupes to 1.10.3

--- manual/jdupes/tools/chocolateyInstall.ps1
+++ manual/jdupes/tools/chocolateyInstall.ps1
@@ -4,11 +4,11 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
-  url           = 'https://github.com/jbruchon/jdupes/releases/download/v1.10.2/jdupes-1.10.2_win32.zip'
-  checksum      = 'c13ab12e53988306660cea822048899c3f277751f9d6205a61dd057097e4d666'
+  url           = 'https://github.com/jbruchon/jdupes/releases/download/v1.10.3/jdupes-1.10.3_win32.zip'
+  checksum      = 'c0da9cc58e49f17e3467a6bb933bd9da9d096aeb237f14e8263dfddd492ca200'
   checksumType  = 'sha256'
-  url64bit      = 'https://github.com/jbruchon/jdupes/releases/download/v1.10.2/jdupes-1.10.2_win64.zip'
-  checksum64    = '15cc8540463053f299c10a16058d0789627c91452e4827ae03e532e7b4f18dc8'
+  url64bit      = 'https://github.com/jbruchon/jdupes/releases/download/v1.10.3/jdupes-1.10.3_win64.zip'
+  checksum64    = 'f98490dc40ef8f38976c7a946da3658f5f07693842defa3d5bc5828ca245b4e0'
   checksumType64= 'sha256'
 }
 
