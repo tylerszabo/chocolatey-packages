94b8e56ea82209f22efe2e87393e356dc699da0f
Ross Smith II <ross@smithii.com>
2019-06-17 12:53:58 -0700

    Fix jdupes URLs

--- manual/jdupes/tools/chocolateyInstall.ps1
+++ manual/jdupes/tools/chocolateyInstall.ps1
@@ -6,10 +6,10 @@ $currentVersion = '1.12'
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
-  url           = "https://github.com/jbruchon/jdupes/releases/download/v${currentVersion}/jdupes-${currentVersion}_win32.zip"
+  url           = "https://github.com/jbruchon/jdupes/releases/download/v${currentVersion}/jdupes-${currentVersion}-win32.zip"
   checksum      = '1fb1a1d6070f2bdd1bafea82fae56938ac907fef9c3214f094ff905df714ed65'
   checksumType  = 'sha256'
-  url64bit      = "https://github.com/jbruchon/jdupes/releases/download/v${currentVersion}/jdupes-${currentVersion}_win64.zip"
+  url64bit      = "https://github.com/jbruchon/jdupes/releases/download/v${currentVersion}/jdupes-${currentVersion}-win64.zip"
   checksum64    = '607a88d6f8f70a0f3d2e94afe84125d207ff2f50f93375ffe7678189787ef125'
   checksumType64= 'sha256'
 }
