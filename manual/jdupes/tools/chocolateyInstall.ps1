afebdb61ecee24132f62ed0ec78d8d7008e8d07d
Tyler Szabo <tyler@szabomail.ca>
2020-09-15 06:18:25 -0700

    Update jdupes to 1.18.2

--- manual/jdupes/tools/chocolateyInstall.ps1
+++ manual/jdupes/tools/chocolateyInstall.ps1
@@ -4,11 +4,11 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
-  url           = "https://github.com/jbruchon/jdupes/releases/download/v1.18.1/jdupes-1.18.1-win32.zip"
-  checksum      = '05e5a7ee40ee6975fdc010fb93c218157d07abff2e2ab8dedbc6086346123079'
+  url           = "https://github.com/jbruchon/jdupes/releases/download/v1.18.2/jdupes-1.18.2-win32.zip"
+  checksum      = '6eb198043c569caec3005e8b3b6687b37735d1b6cf64d908094b5914b1db50b6'
   checksumType  = 'sha256'
-  url64bit      = "https://github.com/jbruchon/jdupes/releases/download/v1.18.1/jdupes-1.18.1-win64.zip"
-  checksum64    = 'de8d61112bbe0a0f99fd376ff56b891f4505c68f1d3fe6af07dc9f92cda9b831'
+  url64bit      = "https://github.com/jbruchon/jdupes/releases/download/v1.18.2/jdupes-1.18.2-win64.zip"
+  checksum64    = '8656713c7d454c35db901f04609710a58e8e1b74308677939fe2e0ce8cb25530'
   checksumType64= 'sha256'
 }
 
@@ -16,5 +16,5 @@ Install-ChocolateyZipPackage @packageArgs
 
 # Remove old versions
 Get-ChildItem -Path (Join-Path -Path $toolsDir -ChildPath "jdupes-*") -Directory |
-  Where-Object { $_.Name -INotLike "jdupes-1.18.1-*" } |
+  Where-Object { $_.Name -INotLike "jdupes-1.18.2-*" } |
   Remove-Item -Recurse
